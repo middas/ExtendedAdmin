af863208fdb9c803efae2a1ff3f17e9e960e05ce
Justin <justin.middas@gmail.com>
2011-12-31 01:24:29 -0700

    filled in most of the logic, still need to decide if i want a region check

--- BankHandler.cs
+++ BankHandler.cs
@@ -2,21 +2,73 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using ExtendedAdmin.DB;
+using TShockAPI;
 
 namespace ExtendedAdmin
 {
     public class BankHandler
     {
-        public void Deposit(string user, int amount)
+        public void Deposit(TSPlayer player, int amount)
         {
+            BankManager manager = new BankManager(TShock.DB);
+
+            var account = manager.GetBalance(player.UserAccountName);
+
+            // piggy back on the raffle manager to get shards
+            RaffleManager raffleManager = new RaffleManager(TShock.DB);
+
+            var shards = raffleManager.GetServerPointAccounts(player.UserAccountName);
+
+            if (shards.Amount < amount)
+            {
+                player.SendMessage("You do not have the required shards.", Color.Red);
+            }
+            else
+            {
+                manager.Deposit(player.UserAccountName, amount);
+
+                ServerPointSystem.ServerPointSystem.Deduct(new CommandArgs("deduct", player, new List<string>() 
+                {
+                    player.UserAccountName,
+                    amount.ToString()
+                }));
+
+                player.SendMessage("You have successfully deposited into your account.", Color.Green);
+            }
         }
 
-        public void Withdraw(string user, int amount)
+        public void Withdraw(TSPlayer player, int amount)
         {
+            BankManager manager = new BankManager(TShock.DB);
+
+            var account = manager.GetBalance(player.UserAccountName);
+
+            if (account.Amount - amount < 0)
+            {
+                player.SendMessage("You do not have enough to withdraw that amount.", Color.Red);
+            }
+            else
+            {
+                manager.Withdraw(player.UserAccountName, amount);
+
+                ServerPointSystem.ServerPointSystem.Award(new CommandArgs("award", player, new List<string>()
+                {
+                    player.Name,
+                    amount.ToString()
+                }));
+
+                player.SendMessage("You have successfully withdrawn from your account.", Color.Green);
+            }
         }
 
-        public void GetBalance(string user)
+        public void GetBalance(TSPlayer player)
         {
+            BankManager manager = new BankManager(TShock.DB);
+
+            var account = manager.GetBalance(player.UserAccountName);
+
+            player.SendMessage(string.Format("Your account has a balance of: {0}", account.Amount), Color.Green);
         }
     }
 }
