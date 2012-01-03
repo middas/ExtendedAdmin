5c77871a5b0b5f29eac385faf95684f76fa89e62
Middas <justin.middas@gmail.com>
2012-01-02 21:29:58 -0700

    version tick

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0102.16")]
-[assembly: AssemblyFileVersion("1.0.0102.16")]
+[assembly: AssemblyVersion("1.0.0102.17")]
+[assembly: AssemblyFileVersion("1.0.0102.17")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
